6c6fbd18f75ef2f79c4601b3353ffd8bfa8d40ce
bottkars <karsten.bott@bottnet.de>
2019-09-10 12:21:56 +0200

    latest

--- PKS-pwsh/PKS-pwsh.psm1
+++ PKS-pwsh/PKS-pwsh.psm1
@@ -59,32 +59,40 @@ function Connect-PKSapiEndpoint {
             
         }  
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12  
-        $client_id = 'PKS_API_'
-  
+        $clientid = 'pks_cli:'
+        $client_encoded = [System.Text.Encoding]::UTF8.GetBytes($clientid)
+        $client_base64 = [System.Convert]::ToBase64String($client_encoded)
+        $headers = @{
+            'content-type'  = "application/x-www-form-urlencoded"
+            'authorization' = "Basic $client_base64"    
+        }          
     }
     Process {
         $Global:PKS_API_BaseUri = $PKS_API_URI
         Write-Verbose $Global:PKS_API_BaseUri
-        # $body= "grant_type=password&username=$($PKS_API_Credentials.UserName)&password=$(($PKS_API_Credentials.GetNetworkCredential()).password)"
         switch ($PsCmdlet.ParameterSetName) {
             'SSO' {
                 if (!$SSOToken) {
-                    $SSOToken = Read-Host -prompt "Please enter your Temporary Code from $PKS_API_BaseUri/uaa/passcode"
+                    $SSOToken = Read-Host -prompt "Please enter your Temporary Code from $($PKS_API_BaseUri):8443/passcode"
                     $body = "grant_type=password&passcode=$SSOToken"
-                    $clientid = "$($client_id):"
                 }
             }
             'USER' {
                 if (!$PKS_API_Credentials) {
-                    $username = Read-Host -Prompt "Please Enter PKS PKS_API_anager username"
+                    $username = Read-Host -Prompt "Please Enter PKS username"
                     $SecurePassword = Read-Host -Prompt "Password for user $username" -AsSecureString
                     $PKS_API_Credentials = New-Object System.Management.Automation.PSCredential($username, $Securepassword)
                 }
-                $clientid = "$($client_id):"
-                $body = "grant_type=password&username=$([System.Web.HttpUtility]::UrlEncode($PKS_API_Credentials.UserName))&password=$([System.Web.HttpUtility]::UrlEncode(($PKS_API_Credentials.GetNetworkCredential()).password))"
+                $password = $([System.Web.HttpUtility]::UrlEncode(($PKS_API_Credentials.GetNetworkCredential()).password))
+                $Body = @{
+                    'grant_type' = "password"
+                    'username'   = $([System.Web.HttpUtility]::UrlEncode($PKS_API_Credentials.username))
+                    #                'token_format' = "opaque"
+                    'password'   = $password
+                }
+   
             }
             'CLIENT' {
-                #   client_id=admin&client_secret=${ADMIN_CLIENT_SECRET}&grant_type=client_credentials&token_format=opaque
                 if (!$PKS_API_ClientCredentials) {
                     $client_id = Read-Host -Prompt "Please Enter PKS PKS_API_anager Client"
                     $client_secret = Read-Host -Prompt "Secret for $client_id" -AsSecureString
@@ -97,19 +105,17 @@ function Connect-PKSapiEndpoint {
                     'client_id'     = $([System.Web.HttpUtility]::UrlEncode($PKS_API_ClientCredentials.username))
                     'token_format'  = "opaque"
                     'client_secret' = $([System.Web.HttpUtility]::UrlEncode(($PKS_API_ClientCredentials.GetNetworkCredential()).password))
-                } #| ConvertTo-Json
+                } 
+                $headers = @{'content-type' = "application/x-www-form-urlencoded;charset=utf-8"
+                    'Accept'                = "application/json"
+
+                }
             }
         }    
 
-        # $client_encoded = [System.Text.Encoding]::UTF8.GetBytes($clientid)
-        # $client_base64 = [System.Convert]::ToBase64String($client_encoded)
         
-        $headers = @{'content-type' = "application/x-www-form-urlencoded;charset=utf-8"
-            'Accept'                = "application/json"
-        }
+
         $VerbosePreference
-        Write-verbose "Using uri $($Global:PKS_API_BaseUri):8443/oauth/token"
-        Write-Verbose "Using Body: $body"
         Write-Verbose ($body | Out-String)
         Write-Verbose ( $headers | Out-String ) 
         try {  
@@ -151,12 +157,72 @@ function Connect-PKSapiEndpoint {
         $Global:PKS_API_Headers = @{
             'Authorization' = "Bearer $($Response.access_token)"
         }
+        $Global:Refresh_token = $Response.Refresh_token
         Write-Host "Connected to PKS_API_ with $($Response.Scope)"
         Write-Output $Response
     }
 }
 
+
+<#curl 'http://localhost/oauth/token' -i -X POST \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -d 'client_id=app&client_secret=appclientsecret&grant_type=refresh_token&token_format=opaque&refresh_token=9655f63edf2e476ebb6abea944111590-r'
+POST /oauth/token HTTP/1.1
+Accept: application/json
+Content-Type: application/x-www-form-urlencoded
+Host: localhost
+
+client_id=app&client_secret=appclientsecret&grant_type=refresh_token&token_format=opaque&refresh_token=9655f63edf2e476ebb6abea944111590-r
+HTTP/1.1 200 OK
+Content-Length: 1140
+Pragma: no-cache
+X-XSS-Protection: 1; mode=block
+X-Frame-Options: DENY
+X-Content-Type-Options: nosniff
+Cache-Control: no-store
+Content-Type: application/json;charset=UTF-8
+
+{
+  "access_token" : "cd29c86b2edf46fba173b3ca4b6687ad",
+  "token_type" : "bearer",
+  "id_token" : "eyJhbGciOiJIUzI1NiIsImprdSI6Imh0dHBzOi8vbG9jYWxob3N0OjgwODAvdWFhL3Rva2VuX2tleXMiLCJraWQiOiJsZWdhY3ktdG9rZW4ta2V5IiwidHlwIjoiSldUIn0.eyJzdWIiOiIxOGFiZjM0YS00Yzc5LTRhMTAtODJmMS1lNjIwMDJlNGVlN2UiLCJhdWQiOlsiYXBwIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC91YWEvb2F1dGgvdG9rZW4iLCJleHAiOjE1Njc4NTgyNTcsImlhdCI6MTU2NzgxNTA1NywiYW1yIjpbInB3ZCJdLCJhenAiOiJhcHAiLCJzY29wZSI6WyJvcGVuaWQiXSwiZW1haWwiOiI0UmZFRk9AdGVzdC5vcmciLCJ6aWQiOiJ1YWEiLCJvcmlnaW4iOiJ1YWEiLCJqdGkiOiIzNmY5NmU4MDBmYjk0ZDcyOTc0ODdmNDY1MTI2YzIyZSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJjbGllbnRfaWQiOiJhcHAiLCJjaWQiOiJhcHAiLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJ1c2VyX25hbWUiOiI0UmZFRk9AdGVzdC5vcmciLCJyZXZfc2lnIjoiODI4YmU2ZWYiLCJ1c2VyX2lkIjoiMThhYmYzNGEtNGM3OS00YTEwLTgyZjEtZTYyMDAyZTRlZTdlIiwiYXV0aF90aW1lIjoxNTY3ODE1MDU3fQ.4VKHbNpoD8p2ee0HWscxpI7IEMqtKihiy5sA2etF7iY",
+  "refresh_token" : "9655f63edf2e476ebb6abea944111590-r",
+  "expires_in" : 43199,
+  "scope" : "scim.userids cloud_controller.read password.write cloud_controller.write openid",
+  "jti" : "cd29c86b2edf46fba173b3ca4b6687ad"
+}
+Request Parameters
+
+Parameter	Type	Constraints	Description
+grant_type	String	Required	the type of authentication being used to obtain the token, in this case refresh_token#>
 #https://api.pks.labbuildr.local:9021/actuator/info
+
+
+function Update-PKSAccessToken {
+    [CmdletBinding()]
+    param(
+        $PKS_API_BaseUri = $Global:PKS_API_BaseUri
+    )
+    $METHOD = "POST"
+    $URI = "$($PKS_API_BaseUri):8443/oauth/token"
+    
+    $Headers = @{ 'content-type' = "application/vnd.spring-boot.actuator.v2+json; charset=UTF-8"
+        'Accept'                 = "application/json"
+    }
+    try {
+        $Response = Invoke-WebRequest -Headers $Headers -Uri $URI -Method $METHOD -SkipCertificateCheck -ContentType "application/vnd.spring-boot.actuator.v2+json; charset=UTF-8"
+    }
+    catch {
+        Get-PKSWebException  -ExceptionMessage $_
+        break
+    }
+    write-verbose ($response | Out-String)
+    write-output $response.content | ConvertFrom-Json
+}
+
+
+
 function Get-PKSactuator_health {
     [CmdletBinding()]
     param(
@@ -325,7 +391,7 @@ function Get-PKScompute_profiles {
     begin {
         $METHOD = "GET"
         $Myself = $MyInvocation.MyCommand.Name.Substring(7)
-        $Myself.Replace('_','-')
+        $Myself.Replace('_', '-')
     }
     process {
         if ($name) {
@@ -342,6 +408,9 @@ function Get-PKScompute_profiles {
 function Get-PKSusages {
     [CmdletBinding()]
     param(
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [string][alias('username', 'un')]$user,
         [Parameter(Mandatory = $false)][ValidateSet('v1beta1')]$apiVersion = 'v1beta1'
     )
     begin {
@@ -349,7 +418,22 @@ function Get-PKSusages {
         $Myself = $MyInvocation.MyCommand.Name.Substring(7)
     }
     process {
-        $URI = "$($Global:PKS_API_BaseUri):9021/$apiversion/$($Myself)"
+        $URI = "$($Global:PKS_API_BaseUri):9021/$apiversion/$($Myself)/$user"
+        $Response += Invoke-PKSapirequest -uri $URI -Method $METHOD | ConvertFrom-Json
+    }    
+    end { Write-Output $Response }
+}
+
+function Get-PKSerror {
+    [CmdletBinding()]
+    param(
+    )
+    begin {
+        $METHOD = "GET"
+        $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    }
+    process {
+        $URI = "$($Global:PKS_API_BaseUri):9021/$($Myself)"
         $Response += Invoke-PKSapirequest -uri $URI -Method $METHOD | ConvertFrom-Json
     }    
     end { Write-Output $Response }
@@ -523,32 +607,113 @@ function Get-PKSclusterBinding {
 }
 
 
-<#
-    function Get-PKSSessions {
-        [CmdletBinding()]
-        param(
-        )
+
+function Get-PKSSessions {
+    [CmdletBinding()]
+    param(
+    )
+    $METHOD = "GET"
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(7) -replace "_", "/").ToLower()
+    # $response = Invoke-WebRequest -Method $Method -Uri $Global:PKS_API_BaseUri/api/v0/$Myself -Headers $Global:PKS_API_Headers
+    $URI = "$Global:PKS_API_BaseUri/api/v0/$Myself/current"
+    $Response = Invoke-PKSapirequest -uri $URI -Method $METHOD
+    ($response | ConvertFrom-Json).session
+}
+
+
+# DELETE /api/v0/sessions
+function Disconnect-PKSsession {
+    [CmdletBinding()]
+    param(
+    )
+    $METHOD = "DELETE"
+    $Body = @{ } | ConvertTo-Json -compress
+    Write-Verbose $Body
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14) -replace "_", "/").ToLower()
+    # $response = Invoke-WebRequest -Method $Method -Uri $Global:PKS_API_BaseUri/api/v0/$Myself -Headers $Global:PKS_API_Headers
+    $URI = "$Global:PKS_API_BaseUri/api/v0/$Myself"
+    $Response = Invoke-PKSapirequest -uri $URI -Method $METHOD -Body $Body
+    ($response | ConvertFrom-Json).session
+}
+
+function Get-PKSquotas {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [string][alias('username', 'un')]$user,
+        [Parameter(Mandatory = $false)][ValidateSet('v1beta1')]$apiVersion = 'v1beta1'
+    )
+    begin {
         $METHOD = "GET"
-        $Myself = ($MyInvocation.MyCommand.Name.Substring(7) -replace "_", "/").ToLower()
-        # $response = Invoke-WebRequest -Method $Method -Uri $Global:PKS_API_BaseUri/api/v0/$Myself -Headers $Global:PKS_API_Headers
-        $URI = "$Global:PKS_API_BaseUri/api/v0/$Myself/current"
-        $Response = Invoke-PKSapirequest -uri $URI -Method $METHOD
-        ($response | ConvertFrom-Json).session
+        $Myself = $MyInvocation.MyCommand.Name.Substring(7)
     }
-
-
-    # DELETE /api/v0/sessions
-    function Disconnect-PKSsession {
-        [CmdletBinding()]
-        param(
-        )
-        $METHOD = "DELETE"
-        $Body = @{ } | ConvertTo-Json -compress
-        Write-Verbose $Body
-        $Myself = ($MyInvocation.MyCommand.Name.Substring(14) -replace "_", "/").ToLower()
-        # $response = Invoke-WebRequest -Method $Method -Uri $Global:PKS_API_BaseUri/api/v0/$Myself -Headers $Global:PKS_API_Headers
-        $URI = "$Global:PKS_API_BaseUri/api/v0/$Myself"
-        $Response = Invoke-PKSapirequest -uri $URI -Method $METHOD -Body $Body
-        ($response | ConvertFrom-Json).session
+    process {
+        $URI = "$($Global:PKS_API_BaseUri):9021/$apiversion/$($Myself)/$user"
+        $Response += Invoke-PKSapirequest -uri $URI -Method $METHOD | ConvertFrom-Json
+    }    
+    end { Write-Output $Response }
+}    
+function New-PKSquotas {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [string][alias('username', 'un')]$user,
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [Int64]$cpu,
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [double][alias('mem')]$memory,
+        [Parameter(Mandatory = $false)][ValidateSet('v1beta1')]$apiVersion = 'v1beta1'
+    )
+    begin {
+        $METHOD = "POST"
+        $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+        $BODY = @{
+            "limit" = @{
+                "cpu"    = $cpu
+                "memory" = $memory
+            }
+            "owner" = $user
+        } | ConvertTo-Json    	
     }
-    #>
+    process {
+        $URI = "$($Global:PKS_API_BaseUri):9021/$apiversion/$($Myself)"
+        $Response += Invoke-PKSapirequest -uri $URI -Method $METHOD -Body $BODY 
+    }    
+    end { Write-Output $Response }
+}   
+
+function Set-PKSquotas {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [string][alias('username', 'un')]$user,
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [Int64]$cpu,
+        [Parameter(Mandatory = $false, ParameterSetName = 'name',
+            ValueFromPipelineByPropertyName = $true)]
+        [double][alias('mem')]$memory,
+        [Parameter(Mandatory = $false)][ValidateSet('v1beta1')]$apiVersion = 'v1beta1'
+    )
+    begin {
+        $METHOD = "PATCH"
+        $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+        $BODY = @{
+            "limit" = @{
+                "cpu"    = $cpu
+                "memory" = $memory
+            }
+            "owner" = $user
+        } | ConvertTo-Json    	
+    }
+    process {
+        $URI = "$($Global:PKS_API_BaseUri):9021/$apiversion/$($Myself)/$user"
+        $Response += Invoke-PKSapirequest -uri $URI -Method $METHOD -Body $BODY 
+    }    
+    end { Write-Output $Response }
+}   
